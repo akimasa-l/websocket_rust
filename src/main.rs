578560866b7a08ec1c4eda7dfb4da698c16778cd
akimasa-l <liumingzheng8@outlook.com>
2022-05-28 19:37:36 +0900

    awsむっず

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use ws::{listen, CloseCode, Handler, Handshake, Message, Result, Sender};
+fn main() {
+      // WebSocketの開始
+      listen("127.0.0.1:8080", |out| Server {
+        out: out,
+        user_name: String::new(),
+    })
+    .unwrap();
+
+    struct Server {
+        out: Sender,
+        user_name: String,
+    }
+    impl Handler for Server {
+        // WebSocketとのコネクション接続を開始した場合
+        fn on_open(&mut self, handshake: Handshake) -> Result<()> {
+            let headers: &Vec<(String, Vec<u8>)> = handshake.request.headers();
+            for (k, v) in headers {
+                if k == "User-Name" {
+                    self.user_name = String::from_utf8(v.to_vec()).unwrap();
+                }
+            }
+            println!("{}",self.user_name);
+            return Ok(());
+        }
+        // メッセージを受信した場合
+        fn on_message(&mut self, message: Message) -> Result<()> {
+            return self.out.broadcast(message);
+        }
+        // WebSocketとのコネクション接続が閉じた場合
+        fn on_close(&mut self, _code: CloseCode, _reason: &str) {
+            
+        }
+    }
+}
